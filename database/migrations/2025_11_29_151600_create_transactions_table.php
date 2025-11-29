31683b0dabbbe2a4b4dfffbffed07967e2066a20
zacky-aditya-saputra <zasaditsa567@gmail.com>
2025-11-29 22:27:09 +0700

    Membuat struktur tabel database inventaris

---
+++ database/migrations/2025_11_29_151600_create_transactions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
+            $table->enum('type', ['in', 'out']);
+            $table->integer('quantity');
+            $table->decimal('total_price', 15, 2)->nullable();
+            $table->date('transaction_date');
+            $table->text('notes')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
